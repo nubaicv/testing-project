4e7453cc96e26922f45e66ca80aa08ce46237ba6
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2019-04-07 12:30:50 -0100

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Controller/HomeController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+    
+    /**
+     *
+     * @Route("/", name="home-page")
+     */
+    public function index() {
+        
+        return $this->render("home.html.twig", [
+            
+        ]);
+    }
+}
