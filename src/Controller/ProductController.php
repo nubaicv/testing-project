ed6a317a14c2fdc92a377a927625708acaef35f6
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2019-04-27 13:10:45 -0100

    Dia 6

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+// src/Controller/ProductsController.php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProductController extends BaseController {
+
+    public function new_product(Request $request) {
+
+        $product = new Product();
+
+        $form = $this->createFormBuilder($product)
+                ->add('name', TextType::class)
+                ->add('description', TextareaType::class)
+                ->add('price', IntegerType::class)
+                ->add('save', SubmitType::class, ['label' => $this->translator->trans('Salvar produto')])
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $product = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+            
+            return $this->redirectToRoute('new-product-created');
+        }
+
+        return $this->render("products/new_product.html.twig", [
+                    "title" => $this->translator->trans("Novo produto"),
+                    "form" => $form->createView(),
+        ]);
+    }
+    
+    public function created() {
+        
+        return $this->render("products/new_product_created.html.twig", [
+                    "title" => $this->translator->trans("Produto criado com sucesso"),
+        ]);
+    }
+
+}
